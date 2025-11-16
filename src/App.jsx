7207a650d98dd5cd5b5f26baafd852335fa7a49b
Xriva18 <kevinguanoluisa2004@gmail.com>
2025-11-16 12:18:51 -0500

    Add Tailwind CSS integration and remove unused App.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
@@ -20,31 +17,10 @@ function App() {
   }, []);
   
 
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <ul>
+      
+      <ul className="list-disc list-inside bg-red-200">
       {vehiculos.map((vehiculo) => (
         <li key={vehiculo.id || vehiculo.name}>{vehiculo.name || vehiculo.marca || JSON.stringify(vehiculo)}</li>
       ))}
